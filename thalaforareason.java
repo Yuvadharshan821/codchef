271aac09fcb6b6775edbec469a610749c4bebc0d
P.Yuvadharshan <yuvadharshan2005@gmail.com>
2024-12-05 16:01:37 +0530

    Add files via upload

---
+++ thalaforareason.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int N=8;
+		if(N==7)
+		{
+		    System.out.println("THALA");
+		}
+        else{
+            System.out.println("SADGE");
+        }
+	}
+}
