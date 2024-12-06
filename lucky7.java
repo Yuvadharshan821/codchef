6d492867e81cd010b2a56f57344ae6ecad966da7
P.Yuvadharshan <yuvadharshan2005@gmail.com>
2024-12-06 11:43:36 +0530

    Add files via upload

---
+++ lucky7.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    System.out.println(s.charAt(7));
+	}
+}
