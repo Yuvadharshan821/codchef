29f2cfd5fd15e4ce67444a9bd1f9385ddb38c6ba
P.Yuvadharshan <yuvadharshan2005@gmail.com>
2024-12-07 15:00:34 +0530

    Add files via upload

---
+++ moneymaking.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class MoneyMakingChef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        int totalMoney = (X * 5000) + (Y * 9800);
+        System.out.println(totalMoney);
+    }
+}
