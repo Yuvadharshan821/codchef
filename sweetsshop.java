03995f0c8170e2b472df2c4c3031c970437ab339
P.Yuvadharshan <yuvadharshan2005@gmail.com>
2024-12-12 18:02:22 +0530

    Create sweetsshop.java

---
+++ sweetsshop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class SweetsShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        int N = scanner.nextInt();
+        int remainingMoney = X - (N * 10);
+        int jalebis = remainingMoney / 20;
+        System.out.println(jalebis);
+        scanner.close();
+    }
+}
